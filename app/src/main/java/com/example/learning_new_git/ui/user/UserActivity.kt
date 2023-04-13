0efe53583bcca79fafdc7bba0dc6e3042e9b7d29
Roshan Barnwal <roshanbarnwal07@gmail.com>
2023-04-13 18:16:51 +0530

    secondpush

---
+++ app/src/main/java/com/example/learning_new_git/ui/user/UserActivity.kt
@@ -0,0 +1,44 @@
+package com.example.learning_new_git.ui.user
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.learning_new_git.ui.user.ui.theme.Learning_new_gitTheme
+
+class UserActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Learning_new_gitTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.White
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Learning_new_gitTheme {
+        Greeting("Android")
+    }
+}
